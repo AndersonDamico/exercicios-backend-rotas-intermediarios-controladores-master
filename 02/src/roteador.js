7cc0b05ab57c09c4c4a5b769df801aaf5dd0534d
AndersonDamico <andersondamico2@gmail.com>
2023-08-01 14:13:44 -0300

    exercicio 02

---
+++ 02/src/roteador.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const { vezDeJogar, consultar, remover, adicionar } = require('./controladores/rodadas');
+
+const rotas = express();
+
+
+
+rotas.get("/",vezDeJogar)
+rotas.get("/consultor",consultar)
+rotas.get("/remover",remover)
+rotas.get("/adicionar",adicionar)
+
+module.exports = rotas
